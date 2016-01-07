fcb31e9483002e85dfa6087d863c6cc479a50cda
AIMARD <be.aimard@gmail.com>
2016-01-07 19:37:05 +0100

    Mise à jour bug
    Suite à un bug détecté, tous les mails de la boite d’envoie sont lu,
    donc on ne voit pas quand on reçoit un nouveau mail.
    Le problème est réglé, lorsque la boite Imap lit un nouveau mail, si
    celui ci n’est pas un message à envoyer, il laisse le flag de message
    non lu

--- SmsTransfertSystem/app/src/main/java/com/corp/juxo/smstransfertsystem/gmail/GMailReceiver.java
+++ SmsTransfertSystem/app/src/main/java/com/corp/juxo/smstransfertsystem/gmail/GMailReceiver.java
@@ -1,4 +1,5 @@
 package com.corp.juxo.smstransfertsystem.gmail;
+
 import android.content.Context;
 import android.content.Intent;
 
@@ -17,6 +18,7 @@ import javax.mail.NoSuchProviderException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
+import javax.mail.search.FlagTerm;
 
 /**
  * Created by Juxo on 30/10/2015.
@@ -27,7 +29,7 @@ public class GMailReceiver {
     private static final String RETURNCHAR = "(\\r|\\n)";
     private static final String BLANKCHAR = "";
     public static final String ENVOIESECURE = "ContenuSpecialTxt -- ";
-    private static final String ENVOIERETOUR = "Re" ;
+    private static final String ENVOIERETOUR = "Re";
     private static final String identificateurEnvoie = "txtmsg";
 
     private Properties properties = null;
@@ -42,99 +44,104 @@ public class GMailReceiver {
 
     private Context mContext;
 
-            public GMailReceiver(Context c, Intent i, String user, String pass) {
-                mContext = c;
-                intentSent = i;
-                properties = new Properties();
-                properties.setProperty("mail.host", "imap.gmail.com");
-                properties.setProperty("mail.port", "995");
-                properties.setProperty("mail.transport.protocol", "imaps");
-                userName = user;
-                password = pass;
-            }
+    public GMailReceiver(Context c, Intent i, String user, String pass) {
+        mContext = c;
+        intentSent = i;
+        properties = new Properties();
+        properties.setProperty("mail.host", "imap.gmail.com");
+        properties.setProperty("mail.port", "995");
+        properties.setProperty("mail.transport.protocol", "imaps");
+        userName = user;
+        password = pass;
+    }
 
-            public void readMails() {
-                ENCOURS=true;
-                session = Session.getInstance(properties,
-                        new javax.mail.Authenticator() {
-                            protected PasswordAuthentication getPasswordAuthentication() {
-                                return new PasswordAuthentication(userName, password);
-                            }
-                        });
-                try {
-                    store = session.getStore("imaps");
-                    store.connect();
-                    inbox = store.getFolder("inbox");
-                    inbox.open(Folder.READ_WRITE);
-                    Message messages[] = inbox.getMessages();
-                    getExtractionContent(messages);
-                    inbox.close(true);
-                    store.close();
-                } catch (NoSuchProviderException e) {
-                    e.printStackTrace();
-                } catch (MessagingException e1) {
-                    e1.printStackTrace();
-                }
-                ENCOURS=false;
-            }
-
-            public void getExtractionContent(Message[] messages) throws MessagingException {
-                String msg ="";
-                for (int i = 0; i < messages.length; i++) {
-                    Message message = messages[i];
-                    //Address[] from = message.getFrom();
-                    if(message.getSubject().contains(identificateurEnvoie)) {
-                        String identificateur = message.getSubject().split(SEPARATEUR)[0];
-                        String phoneNumber = message.getSubject().split(SEPARATEUR)[1];
-                        msg = processMessageBody(message).replaceAll(RETURNCHAR, BLANKCHAR);
-                        if (identificateur.contains(ENVOIERETOUR) && !msg.contains(ENVOIESECURE)) {
-                            new Sms(mContext, msg, phoneNumber);
-                            message.setFlag(Flags.Flag.DELETED, true);
-                        }
+    public void readMails() {
+        ENCOURS = true;
+        session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(userName, password);
                     }
+                });
+        try {
+            store = session.getStore("imaps");
+            store.connect();
+            inbox = store.getFolder("inbox");
+            inbox.open(Folder.READ_WRITE);
+            Message messages[] = inbox.search(new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+            getExtractionContent(messages);
+            inbox.close(true);
+            store.close();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e1) {
+            e1.printStackTrace();
+        }
+        ENCOURS = false;
+    }
+
+    public void getExtractionContent(Message[] messages) throws MessagingException {
+        String msg = "";
+        for (int i = 0; i < messages.length; i++) {
+            Message message = messages[i];
+            System.out.println(message.getSubject());
+            if (message.getSubject().contains(identificateurEnvoie)) {
+                String identificateur = message.getSubject().split(SEPARATEUR)[0];
+                String phoneNumber = message.getSubject().split(SEPARATEUR)[1];
+                msg = processMessageBody(message).replaceAll(RETURNCHAR, BLANKCHAR);
+                if (identificateur.contains(ENVOIERETOUR) && !msg.contains(ENVOIESECURE)) {
+                    new Sms(mContext, msg, phoneNumber);
+                    message.setFlag(Flags.Flag.DELETED, true);
+                }else{
+                    message.setFlag(Flags.Flag.SEEN, false);
+                }
+
+            }else{
+                message.setFlag(Flags.Flag.SEEN, false);
+            }
+        }
+    }
+
+    public String processMessageBody(Message message) {
+        try {
+            Object content = message.getContent();
+            // check for string
+            // then check for multipart
+            if (content instanceof Multipart) {
+                Multipart multiPart = (Multipart) content;
+                return procesMultiPart(multiPart);
+            } else if (content instanceof String) {
+                return (String) content;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MessagingException e1) {
+            e1.printStackTrace();
+        }
+        return "problème pour envoyer le message";
+    }
+
+    public String procesMultiPart(Multipart content) {
+
+        try {
+            int multiPartCount = content.getCount();
+            for (int i = 0; i < multiPartCount; i++) {
+                BodyPart bodyPart = content.getBodyPart(i);
+                Object o;
+
+                o = bodyPart.getContent();
+                if (o instanceof String) {
+                    return (String) o;
+                } else if (o instanceof Multipart) {
+                    procesMultiPart((Multipart) o);
                 }
             }
-
-            public String processMessageBody(Message message) {
-                try {
-                    Object content = message.getContent();
-                    // check for string
-                    // then check for multipart
-                    if (content instanceof Multipart) {
-                        Multipart multiPart = (Multipart) content;
-                        return procesMultiPart(multiPart);
-                    } else if (content instanceof String) {
-                        return (String) content;
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (MessagingException e1) {
-                    e1.printStackTrace();
-                }
-                return "problème pour envoyer le message";
-            }
-
-            public String procesMultiPart(Multipart content) {
-
-                try {
-                    int multiPartCount = content.getCount();
-                    for (int i = 0; i < multiPartCount; i++) {
-                        BodyPart bodyPart = content.getBodyPart(i);
-                        Object o;
-
-                        o = bodyPart.getContent();
-                        if (o instanceof String) {
-                           return (String) o;
-                        } else if (o instanceof Multipart) {
-                            procesMultiPart((Multipart) o);
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (MessagingException e1) {
-                    e1.printStackTrace();
-                }
-                return "probleme pour envoyer le message";
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MessagingException e1) {
+            e1.printStackTrace();
+        }
+        return "probleme pour envoyer le message";
+    }
 
 }
